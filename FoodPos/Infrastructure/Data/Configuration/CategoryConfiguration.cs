f12671ad2c69518aeda5d9ab19ad687bcee1ed39
SamuelLeiva <leiva20152@gmail.com>
2025-09-06 13:23:01 -0500

    initial project

---
+++ FoodPos/Infrastructure/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+            builder.Property(c => c.Id)
+                .IsRequired();
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
---
+++ FoodPos/Infrastructure/Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.Property(p => p.Id)
+                .IsRequired();
+            builder.Property(p => p.Name)
+                .IsRequired();
+            builder.Property(p => p.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+            builder.Property(p => p.ImageUrl)
+                .HasMaxLength(500);
+            builder.Property(p => p.IsActive)
+                .IsRequired();
+
+            builder.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
