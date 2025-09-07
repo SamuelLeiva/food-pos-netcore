600405f23d3654cc7ed59465182894f0a21c594c
SamuelLeiva <leiva20152@gmail.com>
2025-09-07 17:22:01 -0500

    feature: product repository added

---
+++ FoodPos/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductoRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductoRepository(PosContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Product>> GetProductosMasCaros(int cantidad) =>
+                        await _context.Products
+                            .OrderByDescending(p => p.Price)
+                            .Take(cantidad)
+                            .ToListAsync();
+    }
+
+}
