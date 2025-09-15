4ca43a3453dc9b246d0bd4243fb5056d12bac7cc
SamuelLeiva <leiva20152@gmail.com>
2025-09-15 10:57:31 -0500

    feature: Category's DTOs added.

---
+++ FoodPos/API/Dtos/Categories/CategoryAddUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos.Categories;
+
+public class CategoryAddUpdateDto
+{
+    [Required(ErrorMessage = "Category's name is required.")]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters long.")]
+    [MaxLength(500, ErrorMessage = "Name can't be longer than 500 characters.")]
+    public string Name { get; set; }
+}
